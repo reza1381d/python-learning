c1f53392db14f24c21fbb4bf1c082887757cf0af
Reza <rezadastouri01381@gmail.com>
2026-07-07 03:29:07 +0330

    learn pytho variables

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 name = "Reza"
 age = 23
+height = 1.80
+is_programmer = True
+country = "germany"
 
-print("hello", name)
-print ("your age is", age)
+print(name)
+print(age)
+print(height)
+print(is_programmer)
+print(country)
