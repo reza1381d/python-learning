5c34c67b939d46715a8af47e0cabc839b3c61e48
Reza <rezadastouri01381@gmail.com>
2026-07-08 16:45:49 +0330

    practice lists

---
+++ p4.py
@@ -0,0 +1,17 @@
+numbers = [10,20,30,40,50]
+for number in numbers :
+    print(number)
+
+cities = ["arak","shiraz","Tehran"]
+print(cities[0],cities[-1])
+
+fruits = ["apple", "banana"]
+fruits.append("orange")
+print (fruits , len(fruits))
+
+
+total =0
+nums = [5, 10, 15, 20, 25]
+for num in nums :
+    total += num
+print(total)
