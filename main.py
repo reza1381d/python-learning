6ce2efc9d6d7ed5aea4ddcdc2497f5c13211151d
Reza <rezadastouri01381@gmail.com>
2026-07-08 15:28:08 +0330

    complete python lesson2

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-name = "Reza"
-age = 23
-height = 1.80
-is_programmer = True
-country = "germany"
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+height = float(input("How tall are you? "))
+is_programmer = input("Are you a programmer? ").lower() == "yes" or "Yes" or "y" or "Y"
+country = input("Where are you from? ")
 
-print(name)
-print(age)
-print(height)
-print(is_programmer)
-print(country)
+print("Hello", name)
+print("You are", age, "years old")
+if age>18 :
+    print("you are an adult")
+else :
+    print("you are a minor")
+print("You are", height, "tall")
+
+if is_programmer:
+    print("programmer")
+else:
+    print("not programmer")
+
+print("from", country)
